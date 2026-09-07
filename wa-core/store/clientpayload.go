56fa985dfd0755915f4c0b763df2891ed452bcc2
Thruqe <danielpeter0039@gmail.com>
2026-09-07 17:24:36 +0000

    feat(proto): update wa-core protobuf definitions to v2.3000.1046945112

--- wa-core/store/clientpayload.go
+++ wa-core/store/clientpayload.go
@@ -76,7 +76,7 @@ func (vc WAVersionContainer) ProtoAppVersion() *waWa6.ClientPayload_UserAgent_Ap
 }
 
 // waVersion is the WhatsApp web client version
-var waVersion = WAVersionContainer{2, 3000, 1046915616}
+var waVersion = WAVersionContainer{2, 3000, 1046945112}
 
 // waVersionHash is the md5 hash of a dot-separated waVersion
 var waVersionHash = waVersion.Hash()
